b2c3cc8c15633caea0f01911b5b0033cf4c67513
Tumi Snær Gíslason <tumi@smittendating.com>
2022-06-05 00:31:35 +0000

    edit script

--- deploy-moderately.sh
+++ deploy-moderately.sh
@@ -2,13 +2,21 @@ validate_git_state() {
     git diff --quiet || (>&2 echo 'Git directory dirty, exiting' && exit 1)
 }
 
-apply_terraform() {
-    cd $(dirname terraform/lambda/moderately)
-    >&2 terraform apply --auto-approve
+build_and_push_docker() {
+    pushd lambda/moderately
+    >&2 sh lambda/moderately/build-and-push-docker.sh    
+    popd
 }
 
+apply_terraform() {
+    pushd terraform/lambda/moderately
+    >&2 terraform apply --auto-approve
+    popd
+}
+
+
 validate_git_state &&
-sh lambda/moderately/build-and-push-docker.sh &&
+build_and_push_docker &&
 sh set-image-tag.sh &&
 apply_terraform &&
 sh lambda/moderately/restart-lambda.sh
